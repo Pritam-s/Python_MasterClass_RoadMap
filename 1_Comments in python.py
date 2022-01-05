8e1a0da0474f15da36c71a46f876f27e85f61c2a
SaGE1704 <51298487+SaGE1704@users.noreply.github.com>
2022-01-05 21:25:25 +0530

    Comments Initial commit.

---
+++ 1_Comments in python.py
@@ -0,0 +1,9 @@
+#This is the 'Single line' comment in python
+print("single line comments are give by #")
+
+'''This is 
+the Multi-line
+comment in 
+python'''
+print("Multi-line comments are given by  '''  '''   ")
+
