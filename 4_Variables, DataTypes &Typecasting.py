dac79e04c2421d6c0191e2702abacc443241096c
SaGE1704 <51298487+SaGE1704@users.noreply.github.com>
2022-01-06 09:57:01 +0530

    4_Variables, DataTypes & Typecasting in python. Initial commit.

---
+++ 4_Variables, DataTypes &Typecasting.py
@@ -0,0 +1,29 @@
+'''
+1. Variables -
+
+a) A variable name can contain alphabets,digits, and underscores.
+b) A variable name can only start with an alphabet and underscore.
+c) It can't start with an digit.
+d) No white spaces are allowed.
+e) Reserved keywords should not be used.
+
+2. Data Types (Primitive) -
+
+a) Integers - <class 'int'>
+b) Floating point numbers. - <class 'float'>
+c) Strings - <class 'string'>
+d) Boolean - <class 'bool'>
+e) None - 
+
+3. Typecasting -
+
+a) Typecasting is the way to change one data type of any data or variable to another datatype, i.e., it changes the data type of any variable to some other data type.
+b) There are many functions to convert one data type into another type :-
+
+str() -  this function allows us to convert some other data type into a string.
+
+int() - this function allows us to convert some other data type into an integer. For example, str("34") returns 34 which is of type integer (int)
+
+float() - this function allows us to convert some other data type into a floating-point number, i.e., a number with decimals.
+
+'''
