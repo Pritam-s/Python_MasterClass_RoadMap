1b485fbda315cd812f5fa41d33af745fde514769
SaGE1704 <51298487+SaGE1704@users.noreply.github.com>
2022-01-06 17:07:50 +0530

    8_Added Differences between the 4 Data collection data types. 2nd commit.

--- 8_Data Collection Data Types.py
+++ 8_Data Collection Data Types.py
@@ -6,5 +6,15 @@ Type of Data types (Data Collections Types) :-
         2) Tuples - ()
         3) Sets - {}
         4) Dictionaries - {}
+    
+    b) Differences between them :-
+
+        1. List is a collection which is ordered and changeable. Allows duplicate members.
+
+        2. Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+
+        3. Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+
+        4. Dictionary is a collection which is ordered** and changeable. No duplicate members.
 
 '''
