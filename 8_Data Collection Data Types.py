2e45843dd10b08f4df05c4f9fc2e6f0b472adb79
SaGE1704 <51298487+SaGE1704@users.noreply.github.com>
2022-01-06 17:02:35 +0530

    8_The 4 built-in data collections data types. Initial commit.

---
+++ 8_Data Collection Data Types.py
@@ -0,0 +1,10 @@
+'''
+Type of Data types (Data Collections Types) :-
+
+    a) There are 4 built-in types in Python used to store collections of data :-
+        1) Lists - []
+        2) Tuples - ()
+        3) Sets - {}
+        4) Dictionaries - {}
+
+'''
