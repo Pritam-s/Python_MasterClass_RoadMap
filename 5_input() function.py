2f93fb5a23afe5fd61cddf7b05b0a3779e3d88ba
SaGE1704 <51298487+SaGE1704@users.noreply.github.com>
2022-01-06 11:00:55 +0530

    5_Input() function in python. Initial commit.

---
+++ 5_input() function.py
@@ -0,0 +1,22 @@
+'''
+1. input() function - 
+
+This function allows the user to receive input from the keyboard into the program as a string.
+
+input() function always takes input as a string, i.e., if we ask the user to take a number as input, even then, it will take it as a string, and we will have to typecast it into another data type as per the use case.
+
+eg:- input()
+'''
+
+#Example for input() function in python.
+var1 = input("Please enter your good name.\n")
+print("The age entered by the user is", var1)
+
+'''
+Example for input() function with typecasting as the default
+datatype for any input() function is string.
+'''
+
+
+
+
