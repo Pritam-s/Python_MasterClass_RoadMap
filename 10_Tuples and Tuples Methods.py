ed4c71561efd5d4e9efa57b6df197e818a1fdcec
SaGE1704 <51298487+SaGE1704@users.noreply.github.com>
2022-01-08 11:51:55 +0530

    10_Tuples Description &  Methods with hands-on examples. Initial commit.

---
+++ 10_Tuples and Tuples Methods.py
@@ -0,0 +1,17 @@
+'''
+1. Tuples - 
+    
+    A Tuple is an immutable data type in Python. A tuple in python is a collection of elements enclose in () paranthesis. Tuple, once defined, can't be changed, i.e. it's elements or values cannot be altered or manipulated.
+
+NOTE - 
+    * To create a tuple of one/single element, it is necessary to put a comma ',' after that one element like this tup=('element',). because if we only give a single element inside the parenthesis, the python interpreter will interpret it as a single entity.
+
+'''
+
+#Examples :-
+
+tup1 = ("Olof", "kumiko", "Teddy", "Puppy", 1232, 54,68,797,98,797,6876,8768,987.9,79,8464,9877,65461654415,11,213,13544,33216,43262,6,True,None)
+
+print('\n This is the count method and the result is',tup1.count(797)) #count() method
+
+print('\n This is the index method and the result is',tup1.index("Puppy")) #index() method
