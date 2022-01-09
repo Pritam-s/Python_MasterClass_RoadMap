c69b404e75546ab459c3a669c0e62aacc1c716c2
SaGE1704 <51298487+SaGE1704@users.noreply.github.com>
2022-01-09 15:04:53 +0530

    11_Added whole List of Dictionaries built-in methods. 2nd commit.

--- 11_Dictionaries & Dictionaries Methods.py
+++ 11_Dictionaries & Dictionaries Methods.py
@@ -12,6 +12,31 @@
         * It is indexed (after the Python 3.7th update, the compiler stores the entries in the order they are created)
         * No duplication of data allowed
 
+2. Dictionaries Methods :-
+
+    Method	Description
+    
+    clear()	Removes all the elements from the dictionary
+    
+    copy()	Returns a copy of the dictionary
+    
+    fromkeys()	Returns a dictionary with the specified keys and value
+    
+    get()	Returns the value of the specified key
+    
+    items()	Returns a list containing a tuple for each key value pair
+    
+    keys()	Returns a list containing the dictionary's keys
+    
+    pop()	Removes the element with the specified key
+    
+    popitem()	Removes the last inserted key-value pair
+    
+    setdefault()	Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
+    
+    update()	Updates the dictionary with the specified key-value pairs
+    
+    values()	Returns a list of all the values in the dictionary
 '''
 
 #Examples :-
