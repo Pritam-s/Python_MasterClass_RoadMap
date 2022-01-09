3b3b0ca0c665551508ebc30106a84d1344b94844
SaGE1704 <51298487+SaGE1704@users.noreply.github.com>
2022-01-09 15:08:55 +0530

    9_Proper formatting of 'Special Properties' & also for 'Whole list of available Methods'. 6th commit.

--- 9_Lists and Lists Functions.py
+++ 9_Lists and Lists Functions.py
@@ -5,10 +5,10 @@
     b) We can also say that it is a collection of elements from any data type.
 
     c) Special Properties of Lists :
-         List is a collection which is :-
-         ordered.
-         changeable. 
-         Allows duplicate members.
+        * List is a collection which is :-
+        * ordered.
+        * changeable. 
+        * Allows duplicate members.
 
 
 2. Indexing of Lists -
@@ -43,15 +43,25 @@
     Method	    Description
 
     append()	Adds an element at the end of the list
+
     clear()	    Removes all the elements from the list
+
     copy()	    Returns a copy of the list
-    count()	    Returns the number of elements with the specified value
-    extend()	Add the elements of a list (or any iterable), to the end of the current list
-    index()	    Returns the index of the first element with the specified value
+
+    count()	    Returns the number of elements with the specified value.
+
+    extend()	Add the elements of a list (or any iterable), to the end of the current list.
+
+    index()	    Returns the index of the first element with the specified value.
+
     insert()	Adds an element at the specified position
+
     pop()	    Removes the element at the specified position
+
     remove()	Removes the item with the specified value
+
     reverse()	Reverses the order of the list
+
     sort()	    Sorts the list
 '''
 
