b7710e627fc08be4e67640fda844e553bfd6e018
SaGE1704 <51298487+SaGE1704@users.noreply.github.com>
2022-01-09 15:33:53 +0530

    13_Operators in Python. Whole list of operators. Initial commit.

---
+++ 13_Operators.py
@@ -0,0 +1,24 @@
+'''
+1. Operators In Python :-
+
+    Definition - 
+        "Operators in python can be defined as symbols that assist us to perform cerain operations. The operations can be between variable and variable, variable and value, value and value"
+
+
+2. Different Types Of Operators In Python :-
+    
+    a) Arithmetic Operators. (+, -, *, /, %)
+
+    b) Assignment Operators. (=, <<=, +=, -+, * =, etc..)
+
+    c) Comparison Operators. (==, !=, <, >, >=, <=)
+
+    d) Logical Operators. (and, or, not)
+
+    e) Identity Operators. (is, is not)
+
+    f) Member Operators. (in, not in)
+
+    g) Bitwise Operators. (& (AND) , | (OR), ^ (XOR), ~ (NOT), << (Zero fill left shift), >> (Signed right shift) )
+
+'''
