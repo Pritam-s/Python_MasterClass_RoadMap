c9ee2605a31cb25ec49e27142de8d251a8c0051e
SaGE1704 <51298487+SaGE1704@users.noreply.github.com>
2022-01-11 20:31:51 +0530

    18_Break & Continue - Description. Initial commit.

---
+++ 18_Break & Continue Statements.py
@@ -0,0 +1,15 @@
+'''
+1. Break Statement -
+
+    The break statement STOPS THE LOOP even if the while condition is satisfied.
+
+    i.e. In short, it puts a break on the loop and doesn't allow to iterate further.
+    
+
+2. Continue Statement -
+
+    The Continue statement STOPS THE CURRENT ITERATION OF THE LOOP and then continues with the next iteration.
+
+    i.e. in short, it just stops the current iteration and not the whole loop. and also allows to continue with the further iteration.
+
+'''
