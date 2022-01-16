400d07c28d3c9e889a5b3e1f7ee1717149b1227d
SaGE1704 <51298487+SaGE1704@users.noreply.github.com>
2022-01-16 19:24:56 +0530

    16.2_Added the output of various input values. 3rd commit.

--- Programs/16.2_Find the Prime Numbers.py
+++ Programs/16.2_Find the Prime Numbers.py
@@ -1,6 +1,6 @@
 #Prime Numbers 
 
-num = int(input('Please enter the number to find the prime number '))
+num = int(input('Please enter the number to find the prime number till that number - '))
 start = 1
 
 for items in range(start, num+1):
@@ -10,3 +10,32 @@ for items in range(start, num+1):
             count = count+1
     if (count == 2):
         print(items)
+
+
+# O/P :-
+'''
+#1)
+
+Please enter the number to find the prime number till that number - 40
+2
+3
+5
+7
+11
+13
+17
+19
+23
+29
+31
+37
+
+#2)
+
+Please enter the number to find the prime number till that number - 10
+2
+3
+5
+7
+
+'''
