8e6602c9a09545f1b3d4c49ddac070d41fa8ca14
SaGE1704 <51298487+SaGE1704@users.noreply.github.com>
2022-01-16 15:50:31 +0530

    18_Added Use Cases and updated Description.

--- 18_Break & Continue Statements.py
+++ 18_Break & Continue Statements.py
@@ -1,4 +1,7 @@
 '''
+Break & Continue -  
+    In English also, the word 'Break' means Interrupt and the word 'Continue' means resuming after an interrupt.
+
 1. Break Statement -
 
     The break statement STOPS THE LOOP even if the while condition is satisfied.
@@ -12,4 +15,12 @@
 
     i.e. in short, it just stops the current iteration and not the whole loop. and also allows to continue with the further iteration.
 
+
+3. Use Cases of 'Continue' & 'Break' -
+
+    When you are working with a big project, there might occur a situaion where you only need to use the loop partially wihout adding new or removing already existing lines of code. We can easily apply the break statement in your code so that you can partially run it without any sort of error.
+
+    OR
+
+    In a situation, let's suppose you are working with the while loop printing some lines on the screen and you want to skop an iteration in between others, then you can write the continue statement using a "if" statement that matches your need so that you an skip the specific iteration.
 '''
