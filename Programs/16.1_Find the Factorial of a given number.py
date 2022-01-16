594e47e904bedcea3297dbd5a4ec3b09cf97ca22
SaGE1704 <51298487+SaGE1704@users.noreply.github.com>
2022-01-16 19:14:24 +0530

    16.1_Added the output of various inputs. 2nd commit.

--- Programs/16.1_Find the Factorial of a given number.py
+++ Programs/16.1_Find the Factorial of a given number.py
@@ -1,9 +1,24 @@
 #Write a Program to find the Factorial a of given number.
 
-num = int(input(f"Please enter a number to find it's factorial - "))
+num = int(input("Please enter a number to find it's factorial - "))
 start = 1
 fact = 1
 
 for items in range(start, num+1):
     fact = fact * items
-print(f'The factorial of the number {items} is {fact}.')
+print(f'The factorial of the given number {items} is {fact}.')
+
+# O/P :-
+'''
+#1)
+Please enter a number to find it's factorial - 4
+The factorial of the given number 4 is 24.
+
+#2)
+Please enter a number to find it's factorial - 5
+The factorial of the given number 5 is 120.
+
+#3)
+Please enter a number to find it's factorial - 6
+The factorial of the given number 6 is 720.
+'''
