c70625518342e66d917ba1235f90bbbeac90fc4d
SaGE1704 <51298487+SaGE1704@users.noreply.github.com>
2022-01-27 20:28:01 +0530

    27_Scope - Added "Nested Function and it's effect on Scope". 2nd commit.

--- 27_Scope - Global Variables, Global Keyword Local Variables.py
+++ 27_Scope - Global Variables, Global Keyword Local Variables.py
@@ -14,11 +14,17 @@
 
 5. Rules of global keyword:
     If we assigned a value to a variable within the function body, it would be local unless explicitly declared as global. 
-    
+
     Those variables that are referenced only inside a function are implicitly global. 
-    
+
     There is no need to use the global keyword outside a function.
 
+6. Nested Function. effect of nesting on scope.
+
+    When we define a function inside another function, it becomes a nested function. We already know how to access a global function from a function by using a global keyword. 
+    When we declare a local variable in a function, its scope is usually restricited to that function alone. 
+    This is because each function and subfunction stores its variables in it sepatate workspace. A nested function also has its own workspace. But it can be accessed to the workspaces of all functions in which it is nested. 
+    A variable whos values is assigned by the primary function can be read or overwritten by a function nested at any level within the primary.
 '''
 
 # Example 1 (Scope)
