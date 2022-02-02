62c02ad90d1cc9448cd962ba57488d9212a1f6e0
SaGE1704 <51298487+SaGE1704@users.noreply.github.com>
2022-02-02 15:56:53 +0530

    24.2_File Renamed + nonlocal keyword and extra examples added. 2nd commit.

--- 24.2_Scope - Global & Local Variables.py
+++ 24.2_Scope - Global & Local Variables.py
@@ -20,38 +20,41 @@
     There is no need to use the global keyword outside a function.
 
 6. Nested Function. effect of nesting on scope.
-
     When we define a function inside another function, it becomes a nested function. We already know how to access a global function from a function by using a global keyword. 
     When we declare a local variable in a function, its scope is usually restricited to that function alone. 
     This is because each function and subfunction stores its variables in it sepatate workspace. A nested function also has its own workspace. But it can be accessed to the workspaces of all functions in which it is nested. 
     A variable whos values is assigned by the primary function can be read or overwritten by a function nested at any level within the primary.
+
+7. Nonlocal Variables
+    The nonlocal keyword is used to work with variables inside nested functions, where the variable should not belong to the inner function.
+    Nonlocal variables are used in nested functions whose local scope is not defined. This means that the variable can be neither in the local nor the global scope.
+    We Use the keyword nonlocal to declare that the variable is not local.
 '''
 
-# Example 1 (Scope)
-num1 = 50 # Global Scope
 
+# 1. Creating a Global Variable (Will be accessible inside as well as outside a function)
+var1 = 220
+def test():
+    print(f'This num1 {var1} is also accessible inside the function.')
+
+test()
+print(f'This num1 {var1} is also accessible outside the function.')
+#O/P :-
+#This num1 220 is also accessible inside the function.
+#This num1 220 is also accessible outside the function.
+
+
+
+# 2. Creating a Local variable (will be available only inside the function) (Local Scope)
+num1 = 50 #Global Variable
 def func1():
-    num1 = 20
-    num2 = 12
-    print(num1)
+    num1 = 20 #Local var
+    num2 = 12 #Local var
+    print(f'This is the local num1 {num1} and local num2 {num2}')
 
-func1() #Local O/P num1 = 20
-print(num1) #Global O/P num1- 50
+func1() 
+print(f'This is the global num1 {num1}') 
+#O/P - 
+#This is the local num1 20 and local 12
+#This is the global num1 50
 
-
-
-# Example 2 (Nested Function)
-x = 50 # Global Variable
-
-def harry():
-    x=20
-
-    def rohan():
-        global x
-        x=12
-
-    print(x)
-    rohan()
-
-harry() #Local O/P x = 20
-print(x) #Global O/P x = 12
