a56fe868df76b75edf09891763f5411a3fb094df
SaGE1704 <51298487+SaGE1704@users.noreply.github.com>
2022-02-03 17:56:29 +0530

    Program to find the nth fibonacci number. Initial commit.

---
+++ Programs/Program to find the nth fibonacci number.py
@@ -0,0 +1,16 @@
+#Using Recursion CWH 
+# 0 1 1 2 3 5 8 13
+def fibonacci(n):
+    if n==1:
+        return 0
+    elif n==2:
+        return 1
+    else:
+        return fibonacci(n-1)+ fibonacci(n-2)
+
+
+number = int(input("Enter then number"))
+print(fibonacci(number))
+
+
+
