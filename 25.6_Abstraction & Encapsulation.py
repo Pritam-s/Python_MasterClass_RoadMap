316f81b31e186330a07946d492608c69ed751b4a
SaGE1704 <51298487+SaGE1704@users.noreply.github.com>
2022-02-11 00:18:55 +0530

    25.6_Abstraction & Encapsulatin only description and detailed explanation. Initial commit.

---
+++ 25.6_Abstraction & Encapsulation.py
@@ -0,0 +1,30 @@
+#Abstraction & Encapsulation :-
+'''
+1). What is Abstraction ?
+
+    Abstraction refers to hiding unnecessary details to focus on the whole product instead of parts of the project separately. 
+    
+    It is a mechanism that represents the important features without including implementation details. Abstraction helps us in partitioning the program into many independent concepts so we may hide the irrelevant information in the code. It offers the greatest flexibility when using abstract data-type objects in different situations.
+
+    In programming, abstraction cannot be achieved without Encapsulation.
+
+    Example :- A car which has hidden parts such as camshaft, valves, etc.
+
+
+
+2. What is Encapsulation ?
+
+   Encapsulation means hiding under layers. When working with classes and handling sensitive data, global access to all the variables used in the program is not secure. In Encapsulation, the internal representation of an object is generally hidden from the outside to secure the data. 
+
+   Example :- Capsule.
+
+
+
+3. To sum it up :-
+
+   Abstraction and Encapsulation are fundamental concepts of OOP. 
+   
+   Encapsulation takes all the worry away from the user, providing him with just the product that he requires, irrespective of the way it is formed. 
+   
+   Abstraction focuses on the working of the object instead of the how part, while Encapsulation is all about hiding the way or method of working and just providing the working model. 
+'''
